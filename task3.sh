7c963169540eca25e5d980980720ea2e5560117c
Bishal Aryal <bishal.dmarketer@gmail.com>
2022-04-25 21:12:04 +0545

    first commit

---
+++ task3.sh
@@ -0,0 +1,72 @@
+echo "Enter the name of file to create a new file: "
+read new_file
+touch $new_file
+echo "Enter the plaintext message to the file for encryption:"
+read msg_content
+echo $msg_content > $new_file
+clear
+
+function encryption(){
+    echo "Enter the name of file to be encrypted:"
+    read toencFile
+    echo "Enter the encryption algorithm:"
+    read encAlg
+    echo "Enter the password for encryption:"
+    read passEnc
+    echo "Enter the filename that will be used to save as output:"
+    read outFile
+
+openssl enc -$encAlg -pass pass:$passEnc -p -in $toencFile -out $outFile
+
+choices
+}
+
+function decryption(){
+    echo "Enter the name of file to be decrypted:"
+    read todecFile
+    echo "Enter the decryption algorithm:"
+    read decAlg
+    echo "Enter the password for decryption:"
+    read passDec
+    echo "Enter the filename that will be used to save as output:"
+    read outDec
+
+openssl enc -$decAlg -pass pass:$passDec -d -in $todecFile -out $outDec -P
+
+choices
+}
+ 
+function exitTask(){
+    exit
+}
+
+function choices(){
+    echo " "
+     echo "______________________"
+     echo "| SN   | Task        |"
+     echo "|____________________|"
+     echo "| 1.   |  Encryption |"
+     echo "| 2.   |  Decryption |"
+     echo "| 3.   |    Exit     |"
+     echo "|____________________|"
+     echo " "
+
+echo "Enter task name which you want to perform: "
+read task
+if [[ $task == "Encryption" || $task == "encryption" ]]
+    then
+        encryption
+elif [[ $task == "Decryption" || $task == "decryption" ]]
+    then 
+        decryption
+elif [[ $task == "Exit" || $task == "exit" ]]
+    then
+        exitTask
+else 
+    echo "Please select the task from the above table :)"
+fi
+}
+
+choices #calling main function
+
+
